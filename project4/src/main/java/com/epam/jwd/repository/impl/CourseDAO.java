538b0619723b5c9cfe6e84413dc9fc7ea8fd24cd
SofiaT137 <Sonya137@mail.ru>
2021-10-27 16:42:36 +0300

    Fixed course DAo logic

--- project4/src/main/java/com/epam/jwd/repository/impl/CourseDAO.java
+++ project4/src/main/java/com/epam/jwd/repository/impl/CourseDAO.java
@@ -11,136 +11,122 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class CourseDAO implements DAO<Course,Integer> {
 
-    private static final String SQL_SAVE_COURSE = "INSERT INTO course (course_id, name, start_date, end_date) VALUES (?, ?, ?, ?)";
+    private static final String SQL_SAVE_COURSE = "INSERT INTO course (name, start_date, end_date) VALUES (?, ?, ?)";
+    private static final String SQL_ADD_USER_INTO_COURSE = "INSERT INTO user_has_course (user_id, course_id) VALUES (?, ?)";
     private static final String SQL_UPDATE_COURSE_BY_ID = "UPDATE course SET name = ?, start_date = ?, end_date = ? WHERE course_id = ?";
     private static final String SQL_FIND_ALL_COURSE = "SELECT * FROM course";
     private static final String SQL_FIND_COURSE_BY_ID = "SELECT * FROM course WHERE course_id =  ?";
     private static final String SQL_DELETE_COURSE_BY_ID = "DELETE FROM course WHERE course_id = ?";
 
 
-    private ConnectionPool connectionPool = ConnectionPollImpl.getInstance();
+    private final ConnectionPool connectionPool = ConnectionPollImpl.getInstance();
 
     @Override
     public Course save(Course course) {
-        Connection connection = null;
-        PreparedStatement statement;
-        ResultSet resultSet;
-        try{
-            connection = connectionPool.takeConnection();
-            statement = connection.prepareStatement(SQL_SAVE_COURSE);
-            int courseId = statement.executeUpdate();
-            resultSet = statement.getGeneratedKeys();
-            if (resultSet.next()) {
-                course.setId(resultSet.getInt(1));
-            }
-            statement.setString(2,course.getName());
-            statement.setDate(3, (Date) course.getStartCourse());
-            statement.setDate(4, (Date) course.getEndCourse());
-
-        } catch (SQLException | InterruptedException e) {
-            e.printStackTrace();
-        }finally {
-            connectionPool.returnConnection(connection);
+        try(Connection connection = connectionPool.takeConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_SAVE_COURSE);
+            preparedStatement.setString(1,course.getName());
+            preparedStatement.setDate(2, (Date) course.getStartCourse());
+            preparedStatement.setDate(3,(Date) course.getEndCourse());
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            int course_id = resultSet.getInt(1);
+            course.setId(course_id);
+            preparedStatement.close();
+            resultSet.close();
+            return course;
+        } catch (SQLException | InterruptedException exception) {
+            //TODO log and throw exception;
+            return null;
         }
-        return course;
     }
 
-    public User addUser(){
-        // в таблицу User has course добавить запить, кот.будет связывать 2 таблицы
+    public Boolean addUserIntoCourse(Course course, User user){
+        try(Connection connection = connectionPool.takeConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_ADD_USER_INTO_COURSE);
+            preparedStatement.setInt(1,course.getId());
+            preparedStatement.setInt(2,user.getId());
+            Boolean result = preparedStatement.executeUpdate() > 0;
+            preparedStatement.close();
+            return result;
+        } catch (SQLException | InterruptedException exception) {
+            //TODO log and throw exception;
+            return false;
+        }
     }
 
 
     @Override
     public Boolean update(Course course) {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet;
-        try {
-            connection = connectionPool.takeConnection();
-            assert connection != null;
-            connection.prepareStatement(SQL_UPDATE_COURSE_BY_ID);
-            statement.setInt(1,course.getId());
-            statement.setString(2,course.getName());
-            statement.setDate(3, (Date) course.getStartCourse());
-            statement.setDate(4, (Date) course.getEndCourse());
-            return Objects.equals(statement.executeUpdate(),1);
-        } catch (SQLException | InterruptedException e) {
-            //log
+        try(Connection connection = connectionPool.takeConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_UPDATE_COURSE_BY_ID);
+            preparedStatement.setString(1,course.getName());
+            preparedStatement.setDate(2, (Date) course.getStartCourse());
+            preparedStatement.setDate(3,(Date) course.getEndCourse());
+            preparedStatement.setInt(4,course.getId());
+            Boolean result = preparedStatement.executeUpdate() > 0;
+            preparedStatement.close();
+            return result;
+        } catch (SQLException | InterruptedException exception) {
+            //TODO log and throw exception;
             return false;
-        }finally {
-            connectionPool.returnConnection(connection);
         }
     }
     @Override
     public Boolean delete(Course course) {
-        Connection connection = null;
-        PreparedStatement preparedStatement;
-        try {
-            connection = connectionPool.takeConnection();
-            assert connection != null;
-            preparedStatement = connection.prepareStatement(SQL_DELETE_COURSE_BY_ID);
-            preparedStatement.setInt(1, course.getId());
-            return Objects.equals(preparedStatement.executeUpdate(), 1);
+        try(Connection connection = connectionPool.takeConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_DELETE_COURSE_BY_ID);
+            preparedStatement.setInt(1,course.getId());
+            Boolean result = preparedStatement.executeUpdate() > 0;
+            preparedStatement.close();
+            return result;
         } catch (SQLException | InterruptedException exception) {
-            //log
+            //TODO log and throw exception;
             return false;
-        } finally {
-            connectionPool.returnConnection(connection);
         }
     }
 
     @Override
     public List<Course> findAll() {
-        Connection connection = null;
         List<Course> courses = new ArrayList<>();
-        Statement statement;
-        ResultSet resultSet;
-        try {
-            connection = connectionPool.takeConnection();
-            assert connection != null;
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery(SQL_FIND_ALL_COURSE); {
-                while (resultSet.next()) {
-                    courses.add(returnCourse(resultSet));
-                }
+        try(Connection connection = connectionPool.takeConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_FIND_ALL_COURSE);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                courses.add(returnCourse(resultSet));
             }
-        }catch (SQLException | InterruptedException e) {
-            //todo implement logger and custom exception
-        } finally {
-            connectionPool.returnConnection(connection);
+            preparedStatement.close();
+            resultSet.close();
+            return courses;
+        } catch (SQLException | InterruptedException exception) {
+            //TODO log and throw exception;
+            return null;
         }
-        return courses;
     }
 
     @Override
     public Course findById(Integer id) {
-        Connection connection = null;
-        PreparedStatement preparedStatement;
-        ResultSet resultSet;
-        try{
-            connection = connectionPool.takeConnection();
-            assert connection != null;
-            preparedStatement = connection.prepareStatement(SQL_FIND_COURSE_BY_ID);
+        Course course = null;
+        try(Connection connection = connectionPool.takeConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_FIND_COURSE_BY_ID);
             preparedStatement.setInt(1,id);
-            resultSet = preparedStatement.executeQuery();
+            ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
-                return returnCourse(resultSet);
+                course=  returnCourse(resultSet);
             }
-        } catch (SQLException | InterruptedException e) {
-            //  logger
+            preparedStatement.close();
+            resultSet.close();
+        } catch (SQLException | InterruptedException exception) {
+            //TODO log and throw exception;
+            return null;
         }
-        finally {
-            connectionPool.returnConnection(connection);
-        }
-
-        return null;
+        return course;
     }
 
     private Course returnCourse(ResultSet resultSet) {
