b96a3fb10c69533fec88a7ec4ea1902b64e83c18
SofiaT137 <Sonya137@mail.ru>
2021-11-04 14:19:18 +0300

    Added Account validator logic

--- project4/src/main/java/com/epam/jwd/service/validator/impl/AccountValidator.java
+++ project4/src/main/java/com/epam/jwd/service/validator/impl/AccountValidator.java
@@ -1,4 +1,42 @@
 package com.epam.jwd.service.validator.impl;
 
+import java.rmi.ServerException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AccountValidator {
+
+    private static String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$)$";
+    private static String LOGIN_REGEX  = "\\b[a-zA-Z][a-zA-Z0-9\\-._]\\b";
+
+    private final static Integer MIN_PASSWORD_LENGTH = 8;
+    private final static Integer MIN_LOGIN_LENGTH = 4;
+
+    private static final String PASSWORD_LENGTH_EXCEPTION = "The length must be more than 8 character";
+    private static final String LOGIN_LENGTH_EXCEPTION = "The length must be more than 4 character";
+
+    @Override
+    public boolean validate(AccountDTO dto) throws ServerException {
+        return validatePassword(dto.getPassword())
+                && validateLogin(dto.getLogin());
+    }
+
+    private boolean validatePassword(final String password) throws ServerException {
+        if (password.length() < MIN_PASSWORD_LENGTH){
+            throw new ServerException(PASSWORD_LENGTH_EXCEPTION);
+        }
+        Pattern passwordPattern = Pattern.compile(PASSWORD_REGEX);
+        Matcher matcher = passwordPattern.matcher(password);
+        return matcher.find();
+    }
+
+    private boolean validateLogin(final String login) throws ServerException {
+        if (login.length() < MIN_LOGIN_LENGTH){
+            throw new ServerException(LOGIN_LENGTH_EXCEPTION);
+        }
+        Pattern loginPattern = Pattern.compile(LOGIN_REGEX);
+        Matcher matcher = loginPattern.matcher(login);
+        return matcher.find();
+    }
 }
+
--- project4/src/main/java/com/epam/jwd/service/validator/impl/UserValidator.java
+++ project4/src/main/java/com/epam/jwd/service/validator/impl/UserValidator.java
@@ -9,10 +9,11 @@ import java.util.regex.Pattern;
 public class UserValidator implements Validator {
 
     private static String FIRST_AND_LAST_NAME_REGEX = "^[a-zA-Z '.-]*$";
-    private static final String FIRST_NAME_LENGTH_EX = "The length must be from 2 to 20 characters";
+
     private final static Integer MIN_NAME_LENGTH = 2;
     private final static Integer MAX_NAME_LENGTH = 20;
 
+    private static final String NAME_LENGTH_EXCEPTION = "The length must be from 2 to 20 characters";
 
     @Override
     public boolean validate(UserDTO dto) throws ServerException {
@@ -22,7 +23,7 @@ public class UserValidator implements Validator {
 
     private boolean validateName(final String name) throws ServerException {
         if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH){
-            throw new ServerException(FIRST_NAME_LENGTH_EX);
+            throw new ServerException(NAME_LENGTH_EXCEPTION);
         }
         Pattern namePattern = Pattern.compile(FIRST_AND_LAST_NAME_REGEX);
         Matcher matcher = namePattern.matcher(name);
