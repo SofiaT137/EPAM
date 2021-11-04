e348001349750510742fe2f886cbab34e1852ee2
SofiaT137 <Sonya137@mail.ru>
2021-11-04 16:13:16 +0300

    Added Group Dto

---
+++ project4/src/main/java/com/epam/jwd/service/dto/groupdto/GroupDto.java
@@ -0,0 +1,49 @@
+package com.epam.jwd.service.dto.groupdto;
+
+import com.epam.jwd.service.dto.AbstractDto;
+
+import java.util.Objects;
+
+public class GroupDto extends AbstractDto<Integer> {
+
+    private String name;
+
+    public GroupDto(String name) {
+        this.name = name;
+    }
+
+    public GroupDto(Integer id,String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GroupDto)) return false;
+        GroupDto groupDto = (GroupDto) o;
+        return Objects.equals(name, groupDto.getName())
+                && Objects.equals(id,groupDto.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name,id);
+    }
+
+    @Override
+    public String toString() {
+        return "GroupDto{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
