27faec47204f0df6adcdbfb67133b24602f0565b
SofiaT137 <Sonya137@mail.ru>
2021-11-07 11:16:46 +0300

    Refactor Review validator

--- project4/src/main/java/com/epam/jwd/service/validator/impl/ReviewValidator.java
+++ project4/src/main/java/com/epam/jwd/service/validator/impl/ReviewValidator.java
@@ -1,9 +1,8 @@
 package com.epam.jwd.service.validator.impl;
 
 import com.epam.jwd.service.dto.reviewdto.ReviewDto;
+import com.epam.jwd.service.exception.ServiceException;
 import com.epam.jwd.service.validator.api.Validator;
-
-import java.rmi.ServerException;
 
 public class ReviewValidator implements Validator<ReviewDto> {
 
@@ -15,21 +14,21 @@ public class ReviewValidator implements Validator<ReviewDto> {
     private static final String REVIEW_LENGTH_EXCEPTION = "The review must not be longer than 300 characters";
 
     @Override
-    public boolean validate(ReviewDto dto) throws ServerException {
-        return validateGrade(dto.getGrade())
-                && validateReview(dto.getReview());
+    public void validate(ReviewDto dto) throws ServiceException {
+       validateGrade(dto.getGrade());
+       validateReview(dto.getReview());
     }
 
-    private boolean validateGrade(final Integer grade) throws ServerException {
+    private boolean validateGrade(final Integer grade) throws ServiceException  {
         if (grade < MIN_GRADE_VALUE || grade > MAX_GRADE_VALUE){
-            throw new ServerException(GRADE_VALUE_EXCEPTION);
+            throw new ServiceException(GRADE_VALUE_EXCEPTION);
         }
         return true;
     }
 
-    private boolean validateReview(final String review) throws ServerException {
+    private boolean validateReview(final String review) throws ServiceException  {
         if (review.length() > MAX_REVIEW_LENGTH){
-            throw new ServerException(REVIEW_LENGTH_EXCEPTION);
+            throw new ServiceException(REVIEW_LENGTH_EXCEPTION);
         }
         return true;
     }
