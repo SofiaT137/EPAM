438b6a4640c087cc9b9dd137979b13d471a41799
SofiaT137 <Sonya137@mail.ru>
2021-12-03 22:17:04 +0300

    Add sql files

---
+++ project4/SQL/3.create_tables.sql
@@ -0,0 +1,69 @@
+USE `elective1`;
+
+CREATE TABLE `account` (
+  `account_id` int NOT NULL AUTO_INCREMENT,
+  `role_id` int NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  PRIMARY KEY (`account_id`),
+  UNIQUE KEY `login_UNIQUE` (`login`),
+  KEY `fk_account_role1_idx` (`role_id`),
+  CONSTRAINT `fk_account_role1` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `course` (
+  `course_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  PRIMARY KEY (`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=COMPRESSED;
+
+CREATE TABLE `review` (
+  `review_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `course_id` int NOT NULL,
+  `grade` tinyint NOT NULL,
+  `review` text,
+  PRIMARY KEY (`review_id`),
+  KEY `fk_review_course1_idx` (`course_id`),
+  KEY `fk_review_user1_idx` (`user_id`),
+  CONSTRAINT `fk_review_course1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`),
+  CONSTRAINT `fk_review_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `role` (
+  `role_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`role_id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `university_group` (
+  `university_group_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`university_group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `account_id` int NOT NULL,
+  `university_group_id` int NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `fk_user_account1_idx` (`account_id`),
+  KEY `fk_user_university_group1_idx` (`university_group_id`),
+  CONSTRAINT `fk_user_account1` FOREIGN KEY (`account_id`) REFERENCES `account` (`account_id`),
+  CONSTRAINT `fk_user_university_group1` FOREIGN KEY (`university_group_id`) REFERENCES `university_group` (`university_group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `user_has_course` (
+  `user_id` int NOT NULL,
+  `course_id` int NOT NULL,
+  KEY `fk_user_has_course_course1_idx` (`course_id`),
+  KEY `fk_user_has_course_user1_idx` (`user_id`),
+  CONSTRAINT `fk_user_has_course_course1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`),
+  CONSTRAINT `fk_user_has_course_user1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
