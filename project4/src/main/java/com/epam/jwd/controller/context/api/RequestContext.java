12cb7460d539c626cd76710d159270f79bba1c2a
SofiaT137 <Sonya137@mail.ru>
2021-12-14 21:26:30 +0300

    Added javadocs for context

--- project4/src/main/java/com/epam/jwd/controller/context/api/RequestContext.java
+++ project4/src/main/java/com/epam/jwd/controller/context/api/RequestContext.java
@@ -1,9 +1,39 @@
 package com.epam.jwd.controller.context.api;
 
+/**
+ * The context of the request
+ */
 public interface RequestContext {
+    /**
+     * Add attribute to JSP
+     * @param name of the attribute
+     * @param attribute
+     */
     void addAttributeToJSP(String name,Object attribute);
+
+    /**
+     * Get parameter from JSP
+     * @param name
+     * @return String parameter
+     */
     String getParameterFromJSP(String name);
+
+    /**
+     * Add attribute to session
+     * @param name
+     * @param attribute
+     */
     void addAttributeToSession(String name,Object attribute);
+
+    /**
+     * Get Attribute from session
+     * @param name
+     * @return attribute
+     */
     Object getAttributeFromSession(String name);
+
+    /**
+     * Invalidate current session
+     */
     void invalidateCurrentSession();
 }
--- project4/src/main/java/com/epam/jwd/controller/context/api/ResponseContext.java
+++ project4/src/main/java/com/epam/jwd/controller/context/api/ResponseContext.java
@@ -1,6 +1,18 @@
 package com.epam.jwd.controller.context.api;
 
+/**
+ * The context of the request
+ */
 public interface ResponseContext {
+    /**
+     * Get page
+     * @return page
+     */
     String getPage();
+
+    /**
+     *
+     * @return
+     */
     boolean isRedirected();
 }
